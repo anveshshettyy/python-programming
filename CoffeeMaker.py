06d6bed0610d0f9915da9341f28204df2f848bcc
anveshshettyy <thomigang03@gmail.com>
2024-12-25 17:27:44 +0530

    commit

--- CoffeeMaker.py
+++ CoffeeMaker.py
@@ -5,6 +5,7 @@ class Coffee(ABC):
     def cost(self):
         pass
 
+    @abstractmethod
     def description(self):
         pass
 
@@ -12,9 +13,11 @@ class CoffeeDecorator(Coffee):
     def __init__(self,coffee):
         self._coffee = coffee
 
+    @abstractmethod
     def cost(self):
         pass
 
+    @abstractmethod
     def description(self):
         pass
 
@@ -25,14 +28,39 @@ class SimpleCoffee(Coffee):
     def description(self):
         return "Simple Coffee"
     
-class MilkDecorator(Coffee):
+class MilkDecorator(CoffeeDecorator):
     def cost(self):
-        return self.cost() + 2
+        return self._coffee.cost() + 2
     
     def description(self):
-        return self.description() + ",Milk"
+        return self._coffee.description() + ",Milk"
+    
+class SugarDecorator(CoffeeDecorator):
+    def cost(self):
+        return self._coffee.cost() + 3
+    
+    def description(self):
+        return self._coffee.description() + ",Sugar"
+    
+class WippedCream(CoffeeDecorator):
+    def cost(self):
+        return self._coffee.cost() + 5
+    
+    def description(self):
+        return self._coffee.description() + ",Wipped Cream"
     
 if __name__ == "__main__":
     coffee = SimpleCoffee()
     print(f"Description:{coffee.description()}, Cost:${coffee.cost()}")
+
+    coffee = MilkDecorator(coffee)
+    print(f"Description:{coffee.description()}, Cost:${coffee.cost()}")
+
+    coffee = SugarDecorator(coffee)
+    print(f"Description:{coffee.description()}, Cost:${coffee.cost()}")
+
+    coffee = WippedCream(coffee)
+    print(f"Description:{coffee.description()}, Cost:${coffee.cost()}")
+
+    
     
